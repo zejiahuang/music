7ae1136632253765964d59c978229018ec886326
zejiahuang <3626931949@qq.com>
2025-10-02 10:56:13 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
-#include "playlistmanager.h"
-#include "playerwindow.h"
 #include <QApplication>
+#include "playerwindow.h"
+#include "playlistmanager.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
-    PlaylistManager manager;
-    manager.loadPlaylists("./mymusic");
-    manager.scanMusicFolders("./music"， "./mymusic");
-    manager.savePlaylists("./mymusic");
-
+    
     PlayerWindow w;
     w.show();
+    
+    // 修复函数调用，添加正确的参数
+    PlaylistManager manager;
+    manager.scanMusicFolders("./music"， "./mymusic");
+    manager.loadPlaylists("./mymusic");
+    
     return app.exec();
 }

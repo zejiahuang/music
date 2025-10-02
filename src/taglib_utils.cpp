e2be0a70cae01306c64c28a06fe1f6e6079e43fc
zejiahuang <3626931949@qq.com>
2025-10-02 12:21:51 +0800

    Update taglib_utils.cpp

--- src/taglib_utils.cpp
+++ src/taglib_utils.cpp
@@ -11,23 +11,27 @@ SongInfo readAudioMeta(const QString &filePath) {
     TagLib::FileRef f(filePath.toStdString().c_str());
     if (!f.isNull() && f.tag()) {
         TagLib::Tag *tag = f.tag();
-        s.title = QString::fromStdWString(tag->title().toWString());
-        s.artist = QString::fromStdWString(tag->artist().toWString());
-        s.album = QString::fromStdWString(tag->album().toWString());
+        s.title = QString::fromStdString(tag->title().to8Bit(true));
+        s.artist = QString::fromStdString(tag->artist().to8Bit(true));
+        s.album = QString::fromStdString(tag->album().to8Bit(true));
     }
     if (f.audioProperties())
         s.durationMs = f.audioProperties()->length() * 1000;
-    TagLib::MPEG::File mp3File(filePath.toStdString().c_str());
-    if (mp3File.ID3v2Tag()) {
-        auto *tag = mp3File.ID3v2Tag();
-        auto lyricsFrame = tag->frameListMap()["USLT"];
-        if (!lyricsFrame.isEmpty())
-            s.lyrics = QString::fromStdString(lyricsFrame.front()->toString().to8Bit(true));
-        auto picFrames = tag->frameList("APIC");
-        if (!picFrames.isEmpty()) {
-            auto *pic = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame *>(picFrames.front());
-            QByteArray imgData = QByteArray::fromRawData(pic->picture().data(), pic->picture().size());
-            s.cover.loadFromData(imgData);
+    
+    // 只处理MP3文件的ID3v2标签
+    if (filePath.endsWith(".mp3", Qt::CaseInsensitive)) {
+        TagLib::MPEG::File mp3File(filePath.toStdString().c_str());
+        if (mp3File.ID3v2Tag()) {
+            auto *tag = mp3File.ID3v2Tag();
+            auto lyricsFrame = tag->frameListMap()["USLT"];
+            if (!lyricsFrame.isEmpty())
+                s.lyrics = QString::fromStdString(lyricsFrame.front()->toString()。to8Bit(true));
+            auto picFrames = tag->frameList("APIC");
+            if (!picFrames.isEmpty()) {
+                auto *pic = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame *>(picFrames.front());
+                QByteArray imgData = QByteArray::fromRawData(pic->picture().data(), pic->picture().size());
+                s.cover。loadFromData(imgData);
+            }
         }
     }
     return s;

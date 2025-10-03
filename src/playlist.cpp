1753b32ca6d14b8f7fdbd2b0a01de9b880d1e6f8
zejiahuang <3626931949@qq.com>
2025-10-03 09:21:16 +0800

    Create playlist.cpp

---
+++ src/playlist.cpp
@@ -0,0 +1,39 @@
+#include "playlist.h"
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+
+QJsonObject Playlist::toJson() const {
+    QJsonObject obj;
+    obj["name"] = name;
+    QJsonArray arr;
+    for (const auto& s : songs) {
+        QJsonObject so;
+        so["filePath"] = s.filePath;
+        so["title"] = s.title;
+        so["artist"] = s.artist;
+        so["album"] = s.album;
+        so["durationMs"] = QString::number(s.durationMs);
+        so["lyrics"] = s.lyrics;
+        arr.append(so);
+    }
+    obj["songs"] = arr;
+    return obj;
+}
+
+Playlist Playlist::fromJson(const QJsonObject& obj) {
+    Playlist pl;
+    pl.name = obj["name"].toString();
+    for (const auto& v : obj["songs"].toArray()) {
+        QJsonObject so = v.toObject();
+        SongInfo s;
+        s.filePath = so["filePath"].toString();
+        s.title = so["title"].toString();
+        s.artist = so["artist"].toString();
+        s.album = so["album"].toString();
+        s.durationMs = so["durationMs"].toString().toLongLong();
+        s.lyrics = so["lyrics"].toString();
+        pl.songs.append(s);
+    }
+    return pl;
+}

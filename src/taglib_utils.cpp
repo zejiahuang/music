d268814d245e21c859055610a9aa0a5c00624a42
zejiahuang <3626931949@qq.com>
2025-10-03 15:05:16 +0800

    Add files via upload

--- src/taglib_utils.cpp
+++ src/taglib_utils.cpp
@@ -6,21 +6,19 @@
 #include <taglib/attachedpictureframe.h>
 #include <QDebug>
 
-// Helper function: Convert TagLib::String to QString
 QString TStringToQString(const TagLib::String& str)
 {
     return QString::fromUtf8(str.toCString(true));
 }
 
-SongInfo readAudioMeta(const QString& filePath) 
+SongInfo readAudioMeta(const QString& filePath)
 {
     SongInfo s;
     s.filePath = filePath;
     
-    // Use TagLib to read audio metadata
-    TagLib::FileRef f(filePath.toStdString()。c_str());
+    TagLib::FileRef f(filePath.toStdString().c_str());
     if (!f.isNull() && f.tag()) {
-        TagLib::标签* tag = f.tag();
+        TagLib::Tag* tag = f.tag();
         s.title = TStringToQString(tag->title());
         s.artist = TStringToQString(tag->artist());
         s.album = TStringToQString(tag->album());
@@ -30,19 +28,16 @@ SongInfo readAudioMeta(const QString& filePath)
         s.durationMs = f.audioProperties()->lengthInMilliseconds();
     }
     
-    // Only process ID3v2 tags for MP3 files
     if (filePath.endsWith(".mp3", Qt::CaseInsensitive)) {
-        TagLib::MPEG::File mp3File(filePath.toStdString()。c_str());
+        TagLib::MPEG::File mp3File(filePath.toStdString().c_str());
         if (mp3File.ID3v2Tag()) {
             TagLib::ID3v2::Tag* tag = mp3File.ID3v2Tag();
             
-            // Read lyrics
             TagLib::ID3v2::FrameList lyricsFrameList = tag->frameListMap()["USLT"];
             if (!lyricsFrameList.isEmpty()) {
                 s.lyrics = TStringToQString(lyricsFrameList.front()->toString());
             }
             
-            // Read cover image
             TagLib::ID3v2::FrameList picFrames = tag->frameList("APIC");
             if (!picFrames.isEmpty()) {
                 TagLib::ID3v2::AttachedPictureFrame* pic = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame*>(picFrames.front());

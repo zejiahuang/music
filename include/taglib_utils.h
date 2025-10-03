57edbb0b02f04fc8544a4b14b29f08013d9d28e9
zejiahuang <3626931949@qq.com>
2025-10-03 16:04:07 +0800

    Update taglib_utils.h

--- include/taglib_utils.h
+++ include/taglib_utils.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <QString>
 #include "songinfo.h"
+#include <taglib/tstring.h>
 
+QString TStringToQString(const TagLib::String &str);
 SongInfo readAudioMeta(const QString &filePath);

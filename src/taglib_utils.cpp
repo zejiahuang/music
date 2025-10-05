8e56c28d3974bcdae0abea1331015963d15935a3
zejiahuang <3626931949@qq.com>
2025-10-05 19:33:34 +0800

    Update taglib_utils.cpp

--- src/taglib_utils.cpp
+++ src/taglib_utils.cpp
@@ -1,21 +1,32 @@
 #include "../include/taglib_utils.h"
+
+#if defined(ENABLE_TAGLIB)
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
 #include <taglib/mpegfile.h>
 #include <taglib/id3v2tag.h>
 #include <taglib/attachedpictureframe.h>
+#endif
 
-static QString convertString(TagLib::String s)
+static QString convertString(
+#if defined(ENABLE_TAGLIB)
+    TagLib::String s
+#endif
+)
 {
+#if defined(ENABLE_TAGLIB)
     const char* cstr = s.toCString(true);
     return QString::fromUtf8(cstr);
+#else
+    return QString();
+#endif
 }
 
 SongInfo readAudioMeta(const QString& filePath)
 {
     SongInfo s;
     s.filePath = filePath;
-    
+#if defined(ENABLE_TAGLIB)
     TagLib::FileRef f(filePath.toStdString().c_str());
     if (!f.isNull() && f.tag()) {
         TagLib::Tag* tag = f.tag();
@@ -23,21 +34,17 @@ SongInfo readAudioMeta(const QString& filePath)
         s.artist = convertString(tag->artist());
         s.album = convertString(tag->album());
     }
-    
     if (f.audioProperties()) {
         s.durationMs = f.audioProperties()->lengthInMilliseconds();
     }
-    
     if (filePath.endsWith(".mp3", Qt::CaseInsensitive)) {
         TagLib::MPEG::File mp3File(filePath.toStdString().c_str());
         if (mp3File.ID3v2Tag()) {
             TagLib::ID3v2::Tag* tag = mp3File.ID3v2Tag();
-            
             TagLib::ID3v2::FrameList lyricsFrameList = tag->frameListMap()["USLT"];
             if (!lyricsFrameList.isEmpty()) {
                 s.lyrics = convertString(lyricsFrameList.front()->toString());
             }
-            
             TagLib::ID3v2::FrameList picFrames = tag->frameList("APIC");
             if (!picFrames.isEmpty()) {
                 TagLib::ID3v2::AttachedPictureFrame* pic = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame*>(picFrames.front());
@@ -48,6 +55,9 @@ SongInfo readAudioMeta(const QString& filePath)
             }
         }
     }
-    
+#else
+    // 无 TagLib 时，保留最基础信息，避免运行时加载 tag.dll
+    s.durationMs = 0;
+#endif
     return s;
 }
